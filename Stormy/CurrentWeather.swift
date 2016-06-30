9789b4d8e9da5136a187fe6be3f872299bbd73ad
thanhluu <thanhlt90@gmail.com>
2016-06-30 23:11:07 +0700

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Luu Tien Thanh on 6/30/16.
+//  Copyright © 2016 Luu Tien Thanh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: UIImage
+    
+}
---
+++ Stormy/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Luu Tien Thanh on 6/30/16.
+//  Copyright © 2016 Luu Tien Thanh. All rights reserved.
+//
+
+import UIKit
+
+extension CurrentWeather {
+    var temperatureString: String {
+        return "\(Int(temperature))º"
+    }
+    
+    var humidityString: String {
+        let precentageValue = Int(humidity * 100)
+        return "\(precentageValue)%"
+    }
+    
+    var precipitationProbabilityString: String {
+        let precentageValue = Int(precipitationProbability * 100)
+        return "\(precentageValue)%"
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    private let forecastAPIKey = "bed8463ef6e9d72bbdaf0b8767259b91"
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let icon = WeatherIcon.PartlyCloudyDay.image
+        let currentWeather = CurrentWeather(temperature: 56.0, humidity: 1.0, precipitationProbability: 1.0, summary: "Wet and rainy!", icon: icon)
+        
+        display(currentWeather)
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(forecastAPIKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
+        
+        let weatherData = NSData(contentsOfURL: forecastURL!)
+        
+        let json = try! NSJSONSerialization.JSONObjectWithData(weatherData!, options: []) as! [String: AnyObject]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func display(weather: CurrentWeather) {
+        currentTemperatureLabel.text = weather.temperatureString
+        currentPrecipitationLabel.text = weather.precipitationProbabilityString
+        currentHumidityLabel.text = weather.humidityString
+        currentSummaryLabel.text = weather.summary
+        currentWeatherIcon.image = weather.icon
+    }
+}
+
